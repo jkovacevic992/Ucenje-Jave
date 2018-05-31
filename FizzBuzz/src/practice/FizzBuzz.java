54e7114b351169f1f7e9ec997d5f17ff08ac5cfd
Josip Kovačević <38467885+jkovacevic992@users.noreply.github.com>
2018-05-31 11:06:54 +0200

    My version of FizzBuzz

---
+++ FizzBuzz/src/practice/FizzBuzz.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		int i = 1;
+
+		for (i = 1; i <= 100; i++) {
+			if (i % 3 == 0) {
+				if (i % 5 == 0) {
+					System.out.println("FizzBuzz");
+				} else {
+					System.out.println("Fizz");
+				}
+			}
+
+			else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}
