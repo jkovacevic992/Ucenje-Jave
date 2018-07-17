06b4ee14a6b7e01b3fe69dc42968753f1e235107
Josip Kovačević <38467885+jkovacevic992@users.noreply.github.com>
2018-07-17 12:48:33 +0200

    Započeo na radu za ljetni zadatak.

---
+++ LjetniZadatak/src/kovacevic/ljetnizadatak/Start.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kovacevic.ljetnizadatak;
+
+/**
+ *
+ * @author Josip
+ */
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Izbornik izbornik = new Izbornik();
+        izbornik.setVisible(true);
+        
+    }
+    
+}
